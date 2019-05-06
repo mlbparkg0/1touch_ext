4796bf6029b4a99ef459c1c59f2253a75a30a39a
mlbparkg0 <50280207+mlbparkg0@users.noreply.github.com>
2019-05-06 13:01:11 -0400

    Update content.js

--- 1Touch/scripts/content.js
+++ 1Touch/scripts/content.js
@@ -1,6 +1,24 @@
-﻿chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 // 현재 선택된 탭의 HTML내용을 되돌려 보내줌
+    if (request.data=='getHTML') {
+    	var text = document.documentElement.innerHTML
+    	sendResponse({data: text, success: true});
+    }
+    if (request.data=='refresh') {
 
-    var text = document.documentElement.innerHTML
-    sendResponse({data: text, success: true});
+	refresh();
+
+    }
+
 });
+
+function refresh() {
+
+	var likecnt = eval(document.getElementById("likeCnt").innerText)+1
+	document.getElementById("likeCnt").innerText=likecnt
+
+    //chrome.tabs.getSelected(null, function(tab) {
+  	//var code = 'window.location.reload();';
+  	//chrome.tabs.executeScript(tab.id, {code: code});
+    //});
+}
