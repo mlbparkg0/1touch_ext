ef1b6252e4f070858ffa1a25bca4bb7e2a3277af
mlbparkg0 <50280207+mlbparkg0@users.noreply.github.com>
2019-05-06 13:01:50 -0400

    Update extension.js

--- 1Touch/scripts/extension.js
+++ 1Touch/scripts/extension.js
@@ -1,19 +1,28 @@
-﻿var oneLink=''
+var oneLink=''
+var thisid=''
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    var button = document.getElementById('copy');
-    button.addEventListener('click', function () {
+    var btn_copy = document.getElementById('copy');
+    var btn_like = document.getElementById('like');
+
+    btn_copy.addEventListener('click', function () { // copy the link to clipboard
 	document.execCommand('copy')
+    });
+
+    btn_like.addEventListener('click', function () { // 글 추천
+	like()
     });
 
     var text=''
 
     // 현재 선택된 탭의 HTML내용을 요청해서 받아온 후 유툽 비디오 링크를 분석
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {data: text}, function(response) {
+      chrome.tabs.sendMessage(tabs[0].id, {data: 'getHTML'}, function(response) {
 
 	const re = /(iframe)\s+(src).+(youtu).+(iframe)/gi
+	const re2 = /(글번호).+(\/em)/
+	const re3 = /\d+/
 
 	var text=''
 	if (response) {
@@ -21,6 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 
 	text = text.replace(/iframe/g,'iframe\n')
+	thisid = (text.match(re2)+'').match(re3)   // 게시글 아이디
 
 	const matches = text.match(re)
 	oneLink=''
@@ -65,3 +75,62 @@ document.addEventListener('copy', function(e) {
   e.clipboardData.setData('text/plain', oneLink);
   e.preventDefault();
 });
+
+function like() { // 추천 ♡♡♡
+
+  const Http = new XMLHttpRequest();
+  const url='http://mlbpark.donga.com/mp/action.php?m=like&b=bullpen&id='+thisid;
+  Http.open("GET", url);
+  Http.send();
+  Http.onreadystatechange=(e)=>{
+      if (Http.readyState == 4 && Http.status == 200)
+      {
+	var result = Http.responseText
+	if (result=='ok') {
+    	    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      		chrome.tabs.sendMessage(tabs[0].id, {data: 'refresh'}, function(response) {
+      		});
+    	    });
+
+	}
+        else if ( result == 'nologin' )
+        {
+                    alert ( ' 로그인 후 이용해주세요. ') ;
+                    location.href = "" ;
+        }
+        else if ( result == 'snslogin' )
+        {
+                    alert ( ' SNS 가입회원은 사용이 제한됩니다. ') ;
+        }
+        else if ( result == 'reallogin' )
+        {
+                    alert ( ' 실명인증 후 이용해주세요. ') ;
+        }
+        else if ( result == 'duplicate' )
+        {
+                    alert( ' 이미 추천하셨습니다. ' ) ;
+        }
+        else if ( result == 'self' )
+        {
+                    alert ( ' 본인의 글은 추천하실 수 없습니다. ') ;
+        }
+        else if ( result == 'notfound' )
+        {
+                    alert ( ' 존재하지 않는 게시물입니다. ') ;
+        }
+        else if ( result == 'permission' )
+        {
+                    alert ( ' 권한이 없습니다. ') ;
+        }
+        else if ( result == 'lowlevel' )
+        {
+                    alert( ' 회원 가입 후 30일이 지나야 이용가능합니다. ' ) ;
+        }
+        else
+        {
+                    alert( ' 오류가 발생하였습니다. 다시 시도해주세요. ' ) ;
+        }
+
+      }
+  }
+}
